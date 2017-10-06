bd88c1f0afb64e0c1f8c3f004a9dd1b6cb24872b
SebastianFrancus <se813@vp.pl>
2017-10-06 15:42:45 +0200

    gogo

--- js/object.js
+++ js/object.js
@@ -87,7 +87,7 @@ var platnosci = {
         return sumka;
 }
 }
-alert(platnosci.PoliczInoWszystkie());
+//alert(platnosci.PoliczInoWszystkie());
 /*
 alert(platnosci.pokoj1[1])
 alert(platnosci.pokoj3.length)
@@ -106,7 +106,7 @@ function Auto() {
 
 var samochod = new Auto();
 
-alert(samochod.marka);
+//alert(samochod.marka);
 
 //INNY KONSTRUKTOREK
 
@@ -122,3 +122,140 @@ var osoba1 = new Osoba("Brajanek","kek");
 var osoba2 = new Osoba("Dżesika","kek");
 var full_osoba1 = osoba1;
 console.log(full_osoba1);
+
+var hotel = new Object();
+hotel.nazwa = 'Hotel Poznań';
+hotel.pokoje = 100;
+hotel.pokojeZarezerwowane = 24;
+hotel.silownia = true;
+hotel.rodzajePokoi = ['pojedynczy', 'podwojny', 'apartament'];
+hotel.pokojeWolne = function(){
+    return this.pokoje-this.pokojeZarezerwowane;
+}
+
+//console.log(hotel.pokojeWolne());
+
+//**********************************************************
+
+//var tablica = new Array();
+
+function Hotel(nazwa, pokoje, pokojeZarezerwowane){
+    this.nazwa = nazwa;
+    this.pokoje = pokoje;
+    this.pokojeZarezerwowane = pokojeZarezerwowane;
+    this.wolnePokoje = function() {
+        return this.pokoje-this.pokojeZarezerwowane;
+    }
+}
+
+var lech = new Hotel('lech', 45, 30);
+var merkury = new Hotel('merkury', 1000, 500);
+
+/*document.write('Wolne pokoje w hotelu Lech ' + lech.wolnePokoje()+ '<br>');
+document.write('Wolne pokoje w hotelu Merkury ' + merkury.wolnePokoje()+ '');*/
+
+
+
+
+
+//zad. utwórz konstruktor o nazwie pies i parametrach: imie, rasa, waga oraz ulubioneZajecia, ktroe moga miec wiele wartosci. Utworz 3 obiekty o nazwach : azor edi saba.
+
+function Pies(imie, rasa, waga, ulubioneZajecie){
+    this.imie = imie;
+    this.rasa = rasa;
+    this.waga = waga;
+    this.ulubioneZajecie = ulubioneZajecie;
+
+}
+var azor = new Pies('azor','boxer',10,['łapanie kuli', 'zajecie4']);
+var edi = new Pies('edi ','kundel ', 30, ['gonienie Patryka', ' gonienie']);
+var saba = new Pies('saba', 'owczarek', 70, 'zabijanie wiewiórek');
+document.write('Ulubione zajecie azora to: '+ azor.ulubioneZajecie+'<br>')
+document.write('Ulubione zajecie ediego to: '+ edi.ulubioneZajecie+'<br>')
+
+var psy = [azor, edi, saba];
+
+edi.ulubioneZajecie.push('spanie');
+//console.log(edi.ulubioneZajecie);
+//console.log(psy);
+
+var rozmiar;
+for(var i=0;i<psy.length;i++){
+    if(psy[i].waga > 10){
+        rozmiar = 'duzy pies';
+    } else{
+        rozmiar = 'mały pies';
+    }
+    document.write('Pies:' + "<span id='a' style='color:red'>"+psy[i].imie+'</span>' + ' to ' + rozmiar+'<br>');
+}
+
+var tab = ['Jan ', 'Nowak ', 'Poznań'];
+for(var i in tab){
+ //  document.write(tab[i]);
+}
+
+/*for (var i in edi){
+    document.write(edi[i]);
+}*/
+/*for (var x in edi){
+    if(x == 'imie' || x == 'rasa')
+    document.write(edi[x]+'<br>');
+}*/
+
+
+//********************************************************************************
+//prototype
+
+function Pole(a,b){
+    return a*b;
+}
+
+/*
+console.log(Pole.length);
+console.log(Pole.constructor);
+console.log(Pole.prototype);
+*/
+
+
+//*****************************************************************
+
+function Uczen(imie,nazwisko){
+    this.imie= imie;
+    this.nazwisko = nazwisko;
+}
+
+var Adrian = new Uczen('Adrian','Nowak');
+
+Uczen.prototype.narodowosc = 'Polska';
+Uczen.prototype.wyswietlUcznia = function(){
+    return 'Imie: ' + this.imie + ' Nazwisko ' + this.nazwisko + ' Narodowość: ' + this.narodowosc;
+}
+//document.write(Adrian.wyswietlUcznia());
+console.log(Adrian.constructor);
+console.log(Adrian.constructor.prototype.narodowosc);
+
+console.log(Adrian.hasOwnProperty('imie')); //true
+console.log(Adrian.hasOwnProperty('narodowosc')); //false
+
+/*zad.
+Utworz konstruktor rower w ktorym beda wlasciwosci: marka, kolor.Wyświetl wszystkie dane za pomoca metody wyswietlRower. Wykorzystaj for in. Utworz prototype w ktorym bedzie zapisana ilosc kol. */
+
+function Rower(marka, kolor){
+    this.marka = marka;
+    this.kolor = kolor;
+}
+Rower.prototype.iloscKol= 2;
+Rower.prototype.wyswietlRower = function(){
+    for(var i in this){
+        if(i != 'wyswietlRower'){
+                    document.write(this[i]);
+        }
+    }
+}
+
+var giant = new Rower('Giant ','Czarny ');
+giant.wyswietlRower();
+
+
+
+
